64bf967ec9493faf80e9a61b92e8d0e542a515f8
Doodle <23706830+cafedoodle@users.noreply.github.com>
2026-01-07 20:20:36 -0600

    Disable Playwright test

--- reactwithtypescript/tests/e2e/example.spec.ts
+++ reactwithtypescript/tests/e2e/example.spec.ts
@@ -19,14 +19,14 @@ test("user navigates to Info page from home", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "Info Page" })).toBeVisible();
 });
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+// test("get started link", async ({ page }) => {
+//   await page.goto("https://playwright.dev/");
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
+//   // Click the get started link.
+//   await page.getByRole("link", { name: "Get started" }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
-});
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(
+//     page.getByRole("heading", { name: "Installation" })
+//   ).toBeVisible();
+// });
