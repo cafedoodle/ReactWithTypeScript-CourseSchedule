a9860060a7b20b53f993c650ab6ada06c95cbf10
Doodle <23706830+cafedoodle@users.noreply.github.com>
2026-01-07 21:01:35 -0600

    Revert playwright.config.ts

--- reactwithtypescript/playwright.config.ts
+++ reactwithtypescript/playwright.config.ts
@@ -76,6 +76,5 @@ export default defineConfig({
     command: "npm run dev",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000, // 2 minutes
   },
 });
