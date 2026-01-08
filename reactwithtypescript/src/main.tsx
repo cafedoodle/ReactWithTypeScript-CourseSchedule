6d2b995cb847420a945f8b77cf68cd11e23807f5
Doodle <23706830+cafedoodle@users.noreply.github.com>
2026-01-07 21:34:12 -0600

    Test playwright and App Insights

--- reactwithtypescript/src/main.tsx
+++ reactwithtypescript/src/main.tsx
@@ -3,21 +3,21 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.tsx";
-import {
-  AppInsightsContext,
-  AppInsightsErrorBoundary,
-} from "@microsoft/applicationinsights-react-js";
-import { reactPlugin } from "./appInsights";
+// import {
+//   AppInsightsContext,
+//   AppInsightsErrorBoundary,
+// } from "@microsoft/applicationinsights-react-js";
+// import { reactPlugin } from "./appInsights";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AppInsightsErrorBoundary
+    {/* <AppInsightsErrorBoundary
       appInsights={reactPlugin}
       onError={() => <h1>Something went wrong. AppInsights ErrorBoundary</h1>}
     >
-      <AppInsightsContext.Provider value={reactPlugin}>
-        <App />
-      </AppInsightsContext.Provider>
-    </AppInsightsErrorBoundary>
+      <AppInsightsContext.Provider value={reactPlugin}> */}
+    <App />
+    {/* </AppInsightsContext.Provider>
+    </AppInsightsErrorBoundary> */}
   </StrictMode>
 );
